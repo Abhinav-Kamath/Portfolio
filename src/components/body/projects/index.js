41d7009c06a245b34efff94b9b2452e19989b5f4
Abhinav-Kamath <abhinavkamath1@gmail.com>
2021-08-20 01:34:36 +0530

    projects styling done

--- src/components/body/projects/index.js
+++ src/components/body/projects/index.js
@@ -1,7 +1,20 @@
 import React from "react";
-
+import "./projects.css";
+import { projectData } from "../../data/projects.js";
+import ProjectCard from "./project-card";
+import Separator from "../../common/socials/sepator/separator";
 function Projects() {
-  return <div>projects</div>;
+  return (
+    <div className="projects">
+      <Separator />
+      <label classNaame="section-title">Projects</label>
+      <div>
+        {projectData.map((project) => {
+          return <ProjectCard project={project} />;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
---
+++ src/components/data/projects.js
@@ -0,0 +1,20 @@
+export const projectData = [
+  {
+    id: 1,
+    title: "Random1",
+    about: "kjasbdjbsd",
+    tags: ["ReactJs", "Abc", "adasd", "asdasdasd"],
+    demo: "jsjns",
+    github: "sds",
+    image: "jdndn",
+  },
+  {
+    id: 2,
+    title: "Random1",
+    about: "kjasbdjbsd",
+    tags: ["ReactJs", "Abc"],
+    demo: "jsjns",
+    github: "sds",
+    image: "jdndn",
+  },
+];
