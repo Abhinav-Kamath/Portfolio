b5a354ea459cb171403894dda798bb224978d667
Abhinav-Kamath <abhinavkamath1@gmail.com>
2021-08-21 02:58:04 +0530

    skills section styled

--- src/components/body/Skills/index.js
+++ src/components/body/Skills/index.js
@@ -1,7 +1,31 @@
 import React from "react";
-
+import Separator from "../../common/socials/sepator/separator";
+import "./skills.css";
+import { skillsData } from "../../data/skills";
+import SkillCard from "./skill-card";
+import { isCompositeComponent } from "react-dom/cjs/react-dom-test-utils.production.min";
 function Skills() {
-  return <div>skills</div>;
+  const data = skillsData;
+  return (
+    <div className="skills">
+      <Separator />
+      <label className="section-title">Skills</label>
+      <div className="skills-container">
+        {data.map((item) => {
+          return (
+            <div className="skills-section">
+              <label className="skills-section-title">{item.type}</label>
+              <div className="skills-list">
+                {item.list.map((skill) => {
+                  return <SkillCard skill={skill} />;
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
---
+++ src/components/data/skills.js
@@ -0,0 +1,24 @@
+export const skillsData = [
+  {
+    type: "Frontend",
+    list: [
+      {
+        name: "ReactJS",
+        icon: <i class="devicon-react-original colored"></i>,
+      },
+      {
+        name: "Flutter",
+        icon: <i class="devicon-flutter-plain colored"></i>,
+      },
+    ],
+  },
+  {
+    type: "vhvhv",
+    list: [
+      {
+        name: "hbh",
+        icon: "",
+      },
+    ],
+  },
+];
