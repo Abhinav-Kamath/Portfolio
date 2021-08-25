16c2fcbcffc21c305ef9d61a3c8bd63dd04041b0
Abhinav-Kamath <abhinavkamath1@gmail.com>
2021-08-25 20:11:27 +0530

    body done

--- src/components/body/contacts/index.js
+++ src/components/body/contacts/index.js
@@ -1,7 +1,24 @@
 import React from "react";
-
+import Socials from "../../common/socials";
+import Separator from "../../common/socials/sepator/separator";
+import "./contact.csss"
 function Contact() {
-  return <div>constacts</div>;
+  return (
+    <div className="contacts">
+      <Separator />
+      <label className="section-title">Constact</label>Constact
+      <div className="contact-container">
+        <p>Want to get in touch? Conatact me in any of these platforms</p>
+        <div className="contact-left">
+          <Socials />
+        </div>
+        <div className="download">
+          <i class='fi-rr-cloud-download download-icon'></i>
+          <a download href={}>Download Resume</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;
